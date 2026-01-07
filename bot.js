3bc6ce4f43d75875b3e0b5bc1a4461e40eb3b064
TimeToCry <servigoser4@gmail.com>
2026-01-07 18:16:32 +0300

    Create this bot with TP and Sync function

---
+++ bot.js
@@ -0,0 +1,88 @@
+const mineflayer = require('mineflayer');
+const { pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathfinder')
+const Vec3 = require('vec3');
+const bot = mineflayer.createBot({
+    version: '1.8.8',
+    username: 'for_testing',
+    host: 'localhost',
+    port: '56259'
+})
+
+bot.loadPlugin(pathfinder)
+let botFunc = ''
+let playerCom = -1
+let outMessage = ''
+const silentlog = true //if you want to start bot on public server
+const Owner = 'TimeToCry' //your nick for ^^^^
+let tpTo = new Vec3()
+let distance = new Vec3()
+let sync = false
+let stopVel = false
+let v1 = new Vec3()
+bot.on('spawn', spawnSay)
+bot.on('physicsTick', main)
+bot.on('chat', chatfunc)
+
+function spawnSay() {
+    if (silentlog === true) {
+        bot.chat("/tell " + Owner + ' I spawn on server on coord: ' + Math.round((bot.entity.position.x * 10) / 10) + ' ' + Math.round((bot.entity.position.y * 10) / 10) + ' ' + Math.round((bot.entity.position.z * 10) / 10))
+    } else {
+        bot.chat('I spawn on server on coord: ' + Math.round((bot.entity.position.x * 10) / 10) + ' ' + Math.round((bot.entity.position.y * 10) / 10) + ' ' + Math.round((bot.entity.position.z * 10) / 10))
+    }
+}
+function main() {
+    if (botFunc === 'sync') {
+        const pOwner = bot.players[Owner]
+        const OwnerEnt = pOwner.entity
+        if (!OwnerEnt) return
+        bot.entity.position = OwnerEnt.position.add(OwnerEnt.velocity)
+        bot.entity.pitch = OwnerEnt.pitch
+        bot.entity.yaw = OwnerEnt.yaw
+    }
+    if (botFunc === 'setVel') {
+        bot.entity.velocity = new Vec3(0, 0, 0)
+    }
+    if (botFunc === 'tp') {
+        let botPosition = bot.entity.position
+        const maxTpDistance = 5
+        const [_, params] = outMessage.split(' ')
+        if (!tpTo) {tpTo.set(x, y, z) = params.split(':')}
+        distance = botPosition.distanceTo(tpTo)
+        if (distance < 0.1) {
+            botPosition = tpTo
+            botFunc = ''
+            bot.chat('complete') 
+        }
+        if (distance <= maxTpDistance) {
+            botPosition = tpTo
+            botFunc = ''
+            bot.chat('complete')
+        }
+        v1 = tpTo.subtract(botPosition)
+        botPosition.x += (v1.x / distance) * maxTpDistance
+        botPosition.y += (v1.y / distance) * maxTpDistance
+        botPosition.z += (v1.z / distance) * maxTpDistance
+    }
+}
+
+function chatfunc(name, message) {
+    if (name === bot.username) return
+    if (name === Owner){
+        if (message === 'setVel'){
+            botFunc = 'setVel'
+        }
+        if (message === 'sync') {
+            botFunc = 'sync'
+        }
+        if (message.incudes('tp')) {
+            botFunc = 'tp'
+            outMessage = message
+        }
+        if (message === 'exit') {
+            botFunc = 'exit'
+        }
+        if (message === 'help') {
+
+        }
+    }
+}
